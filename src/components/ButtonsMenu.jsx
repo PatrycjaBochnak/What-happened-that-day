688543ebe09073fd1abedc0f306aef76b487b243
PatrycjaBochnak <103588081+PatrycjaBochnak@users.noreply.github.com>
2024-10-23 19:56:33 +0200

    router and scroll fix

--- src/components/ButtonsMenu.jsx
+++ src/components/ButtonsMenu.jsx
@@ -1,18 +1,32 @@
 import React from "react";
-import { Link } from "react-scroll";
+import { useLocation, useNavigate } from "react-router-dom";
 
-const ButtonsMenu = ({ source, linkName }) => (
-  <Link
-    className="cursor-pointer text-white hover:text-gray-400"
-    activeClass="active"
-    to={source}
-    spy={true}
-    smooth={true}
-    offset={0}
-    duration={500}
-  >
-    {linkName}
-  </Link>
-);
+const ButtonsMenu = ({ source, linkName }) => {
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    if (location.pathname === "/message") {
+      navigate("/");
+    } else if (location.pathname === "/") {
+      // React scroll on /
+      document.getElementById(source).scrollIntoView({
+        behavior: "smooth",
+      });
+    } else if (location.pathname === "/welcome") {
+      // React router /welcome
+      navigate(source === "events" ? "/events" : "/contact");
+    }
+  };
+
+  return (
+    <span
+      className="cursor-pointer text-white hover:text-gray-400"
+      onClick={handleClick}
+    >
+      {linkName}
+    </span>
+  );
+};
 
 export default ButtonsMenu;
