7caec96f73817df4afb4f756fa6210e346a778e0
PatrycjaBochnak <103588081+PatrycjaBochnak@users.noreply.github.com>
2024-10-29 16:20:30 +0100

    add animations

--- src/components/ButtonsMenu.jsx
+++ src/components/ButtonsMenu.jsx
@@ -20,13 +20,15 @@ const ButtonsMenu = ({ source, linkName }) => {
   };
 
   return (
-    <span
-      className="cursor-pointer text-white hover:text-gray-400"
-      onClick={handleClick}
-    >
-      {linkName}
-    </span>
+    <div className="relative group">
+      <span
+        className="cursor-pointer text-white hover:text-gray-400"
+        onClick={handleClick}
+      >
+        {linkName}
+      </span>
+      <span className="absolute left-0 bottom-0 w-0 h-[2px] bg-pink-600 transition-all duration-300 ease-in-out group-hover:w-full"></span>
+    </div>
   );
 };
-
 export default ButtonsMenu;
